9746d1e278e5f742dff16266f1b87f507a00dc33
Wilson Lau <hello@wilsonplau.com>
2023-04-24 14:24:27 +0900

    feat: finish nested arrays

--- NestedArray/src/NestedArray.sol
+++ NestedArray/src/NestedArray.sol
@@ -13,6 +13,12 @@ contract NestedArray {
      * Example: [[1,2], [3,4]] this should return 1 + 2 + 3 + 4 = 10
      */
     function getNestedSum() public view returns (uint256) {
-        // your code here
+        uint256 sum = 0;
+        for (uint256 i = 0; i < arr.length; i++) {
+            for (uint256 j = 0; j < arr[i].length; j++) {
+                sum += arr[i][j];
+            }
+        }
+        return sum;
     }
 }
--- TicTacToe/src/TicTacToe.sol
+++ TicTacToe/src/TicTacToe.sol
@@ -9,8 +9,33 @@ contract TicTacToe {
            or false if not.
         3. Board contains 1's and 0's elements and it is also a 3x3 nested array.
     */
-
     function isWinning(uint8[3][3] memory board) public pure returns (bool) {
-        // your code here
+        // Check Rows
+        for (uint8 row = 0; row < 3; row++) {
+            bool isAllSame = true;
+            uint8 value = board[row][0];
+            for (uint8 col = 0; col < 3; col++) {
+                if (board[row][col] != value) isAllSame = false;
+            }
+            if (isAllSame) return true;
+        }
+
+        // Check Columns
+        for (uint8 col = 0; col < 3; col++) {
+            bool isAllSame = true;
+            uint8 value = board[0][col];
+            for (uint8 row = 0; row < 3; row++) {
+                if (board[row][col] != value) isAllSame = false;
+            }
+            if (isAllSame) return true;
+        }
+
+        // Check Diagonals
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+            return true;
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+            return true;
+
+        return false;
     }
 }
